1de40fc7a95adc5258954a5bb727b4bf6a40e734
hrku <chinum.upadhyaya@gmail.com>
2024-02-19 12:00:02 +0530

    Setup Init

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/candentech/sowtracker/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comcandentech/sow_tracker/test/unit/controller/Admin.controller"
+], function () {
+	"use strict";
+});
