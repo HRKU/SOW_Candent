1e9aba0a980755a8ef69366f4992e0974b9b70c6
hrku <chinum.upadhyaya@gmail.com>
2024-02-22 15:36:30 +0530

    roles and permissions with create role ui

--- webapp/controller/Admin.controller.js
+++ webapp/controller/Admin.controller.js
@@ -1,15 +1,39 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+	['sap/ui/core/mvc/Controller', 'sap/ui/model/json/JSONModel'],
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+	function (Controller, JSONModel) {
+		'use strict';
 
-        return Controller.extend("com.candentech.sowtracker.controller.Admin", {
-            onInit: function () {
+		return Controller.extend('com.candentech.sowtracker.controller.Admin', {
+			onInit: function () {
+				let oModel = new JSONModel({});
 
-            }
-        });
-    });
+				// Load roles from local JSON file
+				$.getJSON('../Roles.json', function (roles) {
+					oModel.setProperty('/roles', roles);
+				});
+
+				this.getView().setModel(oModel);
+
+				// ... other logic from previous response
+			},
+			createRole: function () {
+				this.pDialog ??= this.loadFragment({
+					name: 'com.candentech.sowtracker.view.AddRole',
+				});
+
+				this.pDialog.then((oDialog) => oDialog.open());
+			},
+
+			onCancelRolePress: function () {
+				let oDialog = this.byId('addRoleDialog');
+
+				if (oDialog) {
+					oDialog.close();
+				}
+			},
+		});
+	},
+);
