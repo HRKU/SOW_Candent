fb0912531fad22e3fca588718907c32de0f0cc88
Lalit Jadhao <ljadhao378@gmail.com>
2024-03-15 17:45:55 +0530

    just login api

--- webapp/enum/services.js
+++ webapp/enum/services.js
@@ -1,8 +1,8 @@
 sap.ui.define(
 	// "http://excavator:8000/sow_candent_api/login/","http://yw:8000/sow_candent_api/login/"
 	{
-		login: "http://yw:8000/sow_candent_api/login/",
-		login1: "http://excavator:8000/sow_candent_api/login/",
+		// login: "http://yw:8000/sow_candent_api/login/",
+		login: "http://excavator:8000/sow_candent_api/login/",
 		delete: "http://excavator:8000/sow_candent_api/agreements/delete/",
 		// list: "http://excavator:8000/sow_candent_api/agreements/list",
 		// uploadexcel: "http://excavator:8000/sow_candent_api/upload_excel/",
